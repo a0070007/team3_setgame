7d67d96a54887650eb81aaeee88c4263b6d13d72
a0070007 <estherxie@Esthers-MacBook.local>
2015-07-31 17:51:09 +0800

    update v7

--- team3_setgame/src/java/iss/sa40/team3/rest/GameResource.java
+++ team3_setgame/src/java/iss/sa40/team3/rest/GameResource.java
@@ -1,9 +1,13 @@
 package iss.sa40.team3.rest;
 
 import iss.sa40.team3.business.CardBean;
+import iss.sa40.team3.model.Card;
 import iss.sa40.team3.model.Game;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -21,6 +25,17 @@ public class GameResource {
             @PathParam("title") String title, 
             @PathParam("duration")String duration, 
             @PathParam("maxPlayers") String maxPlayers){
+        
+        List<Card> table = new ArrayList<Card>();
+        List<Card> deck = cardBean.getShuffledDeck();
+        List<List<Card>> list = cardBean.issueCards(deck, table, 12);
+        deck = (List<Card>) list.get(0);
+        table = (List<Card>) list.get(1);
+        
+        //include null check for title, duration and maxPlayers
+        //convert duration from string to time
+        Game game = new Game(title, duration, deck, table, maxPlayers);
+        
         
         return null;
     }
