7368ff4e2766133cd6fcfe7c326ca4598591b12b
a0135523 <a0135523@u.nus.edu>
2015-08-01 15:01:02 +0800

    adding the selectable to the game.html

--- team3_setgame/web/game.js
+++ team3_setgame/web/game.js
@@ -1,3 +1,35 @@
-$(function() {
-    $( "#selectable" ).selectable();
-  });
+$(document).ready(function () {
+    //This is used to size the the card squareness
+    $("#selectable li").css("height", $("#selectable li").css("width"));
+});
+$(function () {
+    var cardtemplate = Handlebars.compile($("#cardtemplate").html());
+    chosencards = new Array();
+
+    $("#btn_submit").on("click", function () {
+        alert(chosencards.toString());
+    });
+
+    $("#selectable").bind("mousedown", function (e) {
+        e.metaKey = true;
+    }).selectable({
+        stop: function () {
+            $(".ui-selected", this).each(function () {
+                if (!chosencards.contain($(this).attr("id")))
+                chosencards.push($(this).attr("id"));
+            });
+        }
+    });
+
+//    $("#selectable").selectable({
+//        stop: function () {
+//            $(".ui-selected", this).each(function () {
+//                chosencards.push($(this).attr("id"));
+//                alert($(this).attr("id"));
+//            });
+//        }
+//    });
+
+});
+
+ 
