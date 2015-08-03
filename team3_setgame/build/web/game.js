8f2b72cb7d96532d60db6ffa947705b4d8eba130
a0135523 <a0135523@u.nus.edu>
2015-08-03 23:52:26 +0800

    Update on front end added validation on create player form - weijie @ 03/08/15 2352

--- team3_setgame/build/web/game.js
+++ team3_setgame/build/web/game.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     //This is used to size the the card squareness
     $("#selectable li").css("height", $("#selectable li").css("width"));
 });
+
 $(function () {
     var cardtemplate = Handlebars.compile($("#cardtemplate").html());
     chosencards = new Array();
@@ -15,11 +16,23 @@ $(function () {
     }).selectable({
         stop: function () {
             $(".ui-selected", this).each(function () {
-                if (!isInArray($(this).attr("id"),chosencards))
-                chosencards.push($(this).attr("id"));
+                if (!isInArray($(this).attr("id"), chosencards))
+                    chosencards.push($(this).attr("id"));
             });
         }
     });
+});
+
+var youtubeFunc = '';
+var outerDiv = document.getElementById("instructionmodal");
+var youtubeIframe = outerDiv.getElementsByTagName("iframe")[0].contentWindow;
+$('#introVideo').on('hidden.bs.modal', function (e) {
+    youtubeFunc = 'pauseVideo';
+    youtubeIframe.postMessage('{"event":"command","func":"' + youtubeFunc + '","args":""}', '*');
+});
+$('#introVideo').on('shown.bs.modal', function (e) {
+    youtubeFunc = 'playVideo';
+    youtubeIframe.postMessage('{"event":"command","func":"' + youtubeFunc + '","args":""}', '*');
 });
 
  
